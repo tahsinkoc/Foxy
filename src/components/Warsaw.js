cb72430b410d1e1a7f9ed2d6a7c78c466213a79f
Tahsin Özgür KOÇ <tahsinkoc6@gmail.com>
2023-06-11 01:12:32 +0300

    ngrokc

--- src/components/Warsaw.js
+++ src/components/Warsaw.js
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react'
 function Warsaw() {
 
     const [ID, setID] = useState('')
-
-    const [wet, setWet] = useState()
+    const [Status, setStatus] = useState([])
+    const [wet, setWet] = useState('')
 
     function handleReq(par) {
         fetch(`${ID}${par}`)
             .then(res => res.json())
-            .then(resa => console.log(resa))
+            .then(resa => setWet(resa.status))
     }
 
     const [data, setData] = useState('');
@@ -17,6 +17,7 @@ function Warsaw() {
     useEffect(() => {
         const fetchData = async () => {
             try {
+                // setData('Loading..')
                 const response = await fetch('https://1b27-46-196-200-1.eu.ngrok.io/nem');
                 const jsonData = await response.json();
                 setData(jsonData);
@@ -27,6 +28,8 @@ function Warsaw() {
 
         fetchData();
     }, [data]);
+
+
 
     return (
         <div className='flex items-center justify-center flex-col'>
@@ -41,14 +44,127 @@ function Warsaw() {
                 <button onClick={() => handleReq('stop')} className='w-full text-center flex items-center rounded-md my-4 relative md:my-4 transition duration-150 border outline-0 border-darker text-darker py-1 text-[1.1rem] bg-gradient-to-r from-transparent to-transparent  px-6 hover:from-rr hover:to-t hover:text-white mx-2'>
                     <p>CLOSE MOTOR</p>
                 </button>
-                <button onClick={() => handleReq('nem')} className='w-full text-center flex items-center rounded-md my-4 md:my-4 relative transition duration-150 border outline-0 border-darker text-darker py-1 text-[1.1rem] bg-gradient-to-r from-transparent to-transparent  px-6 hover:from-rr hover:to-t hover:text-white mx-2'>
-                    <p>CLOSE</p>
-                </button>
+
             </div>
             <div>
-                <p className='text-white text-2xl'>{data.wet}</p>
+                <p className='text-white text-2xl'>NEM: {
+                    Math.floor(100 - data.wet / 1024 * 100)
+                }%</p>
+            </div>
+            <div>
+                <p className='text-white text-2xl'>{wet}</p>
             </div>
         </div>
+
+        // #include <ESP8266WiFi.h>
+        // #include <ESP8266WiFi.h>
+        // #include <ESP8266WebServer.h>
+        // #include <ArduinoJson.h>
+
+        // #define led 2
+        // #define WetData A0
+        // #define WetPow 13
+
+        // #define RunMotor 5
+        // #define RevMotor 4
+
+        // const char* ssid = "TURKSAT-KABLONET-28C5-2.4G";
+        // const char* password = "id6aalq4nm";
+
+
+
+        // ESP8266WebServer server(80);
+
+        // void handleAcik() {
+        //   StaticJsonDocument<200> jsonDoc;
+        //   jsonDoc["name"] = "paul";
+
+        //   String response;
+        //   serializeJson(jsonDoc, response);
+        //   server.sendHeader("Access-Control-Allow-Origin", "*");
+        //   server.sendHeader("Access-Control-Allow-Methods", "GET");
+        //   server.sendHeader("Content-Type", "application/json");
+        //   server.send(200, "application/json", response);
+        //   digitalWrite(led, HIGH);
+        // }
+
+        // void handleWet() {
+        //   digitalWrite(WetPow, HIGH);
+        //   int nema = analogRead(WetData);
+        //   Serial.println(nema);
+        //   StaticJsonDocument<200> jsonDoc;
+        //   jsonDoc["wet"] = nema;
+        //   String response;
+        //   serializeJson(jsonDoc, response);
+
+        //   server.sendHeader("Access-Control-Allow-Origin", "*");
+        //   server.sendHeader("Access-Control-Allow-Methods", "GET");
+        //   server.sendHeader("Content-Type", "application/json");
+        //   server.send(200, "application/json", response);
+
+
+        //   digitalWrite(led, LOW);
+
+        // }
+
+        // void handleMotor(String deg) {
+        //   String status = "";
+        //   if(deg == "ac"){
+        //     digitalWrite(RunMotor, HIGH);
+        //     digitalWrite(RevMotor, LOW);
+        //     status = "Motor Running";
+
+        //   } else {
+        //     digitalWrite(RunMotor, LOW);
+        //     digitalWrite(RevMotor, LOW);
+        //     status = "Motor Stopped";
+        //   }
+        //   StaticJsonDocument<200> jsonDoc;
+        //     jsonDoc["status"] = status;
+        //     String response;
+        //     serializeJson(jsonDoc, response);
+
+        //     server.sendHeader("Access-Control-Allow-Origin", "*");
+        //     server.sendHeader("Access-Control-Allow-Methods", "GET");
+        //     server.sendHeader("Content-Type", "application/json");
+        //     server.send(200, "application/json", response);
+        // }
+
+        // void setup() {
+        //   Serial.begin(115200);
+
+        //   pinMode(WetPow, OUTPUT);
+        //   pinMode(WetData, INPUT);
+        //   pinMode(led, OUTPUT);
+        //   pinMode(RunMotor, OUTPUT);
+        //   pinMode(RevMotor, OUTPUT);
+
+        //   WiFi.begin(ssid, password);
+        //   while (WiFi.status() != WL_CONNECTED) {
+        //     delay(1000);
+        //     Serial.println("Connecting to WiFi...");
+        //   }
+
+        //   Serial.println("Connected to WiFi");
+
+        //   server.on("/acik", handleAcik);
+        //   server.on("/nem", handleWet);
+        //   server.on("/run", [](){
+        //     handleMotor("ac");
+        //   });
+        //   server.on("/stop", [](){
+        //     handleMotor("stop");
+        //   });
+        //   server.begin();
+        //   Serial.println("HTTP server started");
+        // }
+
+
+        // void loop() {
+        //   server.handleClient();
+        // }
+
+
     )
 }
 
